fe1583b2488d8b01709afc3085e413c3e56d65dc
Stefan Kieszkowski <85728496+stefankiesz@users.noreply.github.com>
2025-12-22 14:40:40 -0500

    Release 1.2.0 (#114)
    * Add Instructions for App Client Authentication without Client Secret (#111)
    
    * Fix numbering in and add no-secret case directions to README installation instructions
    
    * Cleanup spacing
    
    * Update referenced step number
    
    * Dual-Stack Endpoint Support (#112)
    
    * Add UI elements for endpoint type selection, cleanup spacing of elements
    
    * Improve endpoint switch UI
    
    * Add handling of the dual-stack UI switch
    
    * Add styling and animation for switch selection
    
    * Add storyboard changes, fix typo and syntax
    
    * Add dual-stack stun server
    
    * Improve UI
    
    * Fix text color for dark-themed devices
    
    * Remove non-supported CI OS versions
    
    * Update CI workflow with new macOS and Xcode versions
    
    * Update app version to 1.2.0 (#113)

--- Swift/KVSiOSApp/Constants.swift
+++ Swift/KVSiOSApp/Constants.swift
@@ -12,7 +12,7 @@ let cognitoIdentityPoolId = "REPLACEME"
 
 // App constants
 let appName = "aws-kvs-webrtc-ios-client"
-let appVersion = "1.1.0"
+let appVersion = "1.2.0"
 
 // KinesisVideo constants
 let awsKinesisVideoKey = "kinesisvideo"
@@ -59,3 +59,7 @@ let awsAccessKey: String? = ProcessInfo.processInfo.environment["AWS_ACCESS_KEY_
 let awsSecretKey: String? = ProcessInfo.processInfo.environment["AWS_SECRET_ACCESS_KEY"]
 let awsSessionToken: String? = ProcessInfo.processInfo.environment["AWS_SESSION_TOKEN"]
 let kvsControlPlaneOverride: String? = ProcessInfo.processInfo.environment["CONTROL_PLANE_URI"]
+
+// Dual-stack endpoint formats.
+let PROD_CONTROL_PLANE_ENDPOINT_FORMAT_DUAL_STACK = "https://kinesisvideo.%@.api.aws"
+let PROD_CONTROL_PLANE_ENDPOINT_FORMAT_DUAL_STACK_CN = "https://kinesisvideo.%@.api.amazonwebservices.com.cn"
